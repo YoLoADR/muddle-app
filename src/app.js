1ac6ae84364360a3c6f44753744adccb7f882975
13091987Y <atticot.yohann@gmail.com>
2017-12-26 17:19:16 +0100

    Removing Individual Options and simply function

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ class MuddleApp extends React.Component {
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
     this.state = {
       options: []
     };
   }
   handleDeleteOptions() {
-    this.setState(() => {
-      return {
-        options: []
-      };
-    });
+    this.setState(() => ( {options: []} ));
+  }
+  // FILTER LOGIC : If it's not equal, he stay in the array else it's not
+  handleDeleteOption(optionToRemove) {
+    this.setState((prevState) => ( {options: prevState.options.filter((option)=> optionToRemove !== option)} ));
   }
   handlePick() {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
@@ -28,19 +29,14 @@ class MuddleApp extends React.Component {
     } else if (this.state.options.indexOf(option) > -1) {
       return 'This option already exist';
     }
-    this.setState((prevState) => {
-      return {
-        options: prevState.options.concat([option])
-      };
-    });
+    this.setState((prevState) => ( {options: prevState.options.concat([option])}));
   }
   render() {
-    const title = 'Indecision';
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -48,6 +44,7 @@ class MuddleApp extends React.Component {
         <Options
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption handleAddOption={this.handleAddOption} />
       </div>
@@ -59,10 +56,14 @@ const Header = function (props) {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   );
 }
+
+Header.defaultProps = {
+  title: 'Je suis un titre par default'
+};
 
 const Action = function (props) {
   return (
@@ -82,7 +83,11 @@ const Options = function(props){
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
       {
-        props.options.map((option) => <Option key={option} optionText={option} />)
+        props.options.map((option) => <Option
+         key={option}
+         optionText={option}
+         handleDeleteOption={props.handleDeleteOption}
+         />)
       }
     </div>
   );
@@ -92,6 +97,9 @@ const Option = function (props) {
   return (
     <div>
       {props.optionText}
+      <button onClick={(e)=>{
+        props.handleDeleteOption(props.optionText);
+      }}>Remove</button>
     </div>
   );
 };
@@ -110,7 +118,7 @@ class AddOption extends React.Component {
 
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
-    this.setState(() => { return { error } });
+    this.setState(() => ({ error } ));
 
 
     e.target.elements.option.value = "";
