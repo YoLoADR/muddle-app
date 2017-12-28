0db3044ef598b1dac7ebf294689e4386eb5658dc
13091987Y <atticot.yohann@gmail.com>
2017-12-28 10:29:20 +0100

    Setting up Babel with Webpack

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
-import './utils';
-console.log('app.js is running yet yet');
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>THIS IS JSX FROM WEBPACK</p>;
+ReactDOM.render(template, document.getElementById('app'));
