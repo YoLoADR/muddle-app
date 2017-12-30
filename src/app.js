58cbbde45094966218ba29c578e2e04db8ce3582
13091987Y <atticot.yohann@gmail.com>
2017-12-30 11:51:12 +0100

    adding and setting simply css with WebPack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import MuddleApp from './components/MuddleApp';
+import './styles/style.css'
 
 ReactDOM.render(<MuddleApp />, document.getElementById('app'));
