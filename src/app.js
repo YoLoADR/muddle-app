141da3b1e5e387051b23198c31343250c302a46e
13091987Y <atticot.yohann@gmail.com>
2018-01-01 10:56:03 +0100

    installing normalize.css inside project

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuddleApp from './components/MuddleApp';
+import 'normalize.css/normalize.css'
 import './styles/style.scss'
 
 ReactDOM.render(<MuddleApp />, document.getElementById('app'));
